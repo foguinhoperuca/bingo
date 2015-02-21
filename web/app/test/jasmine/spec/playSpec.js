d46d2373962111c3f7148be45a831542d41d81a4
Jefferson Campos <foguinho.peruca@gmail.com>
2015-02-21 13:48:55 -0200

    Tests
    Removed showDeck to a named function

--- web/app/test/jasmine/spec/playSpec.js
+++ web/app/test/jasmine/spec/playSpec.js
@@ -5,40 +5,38 @@ define([
 	// , 'text!../../../../../../../test/data/timesheet/noDaysLater.json'
 	, 'text!../../../../../../../data/klarosk.json'
 ], function (_, App, BingoCardCollection, Klarosk) {
+	function printDeck() {
+		var i,
+			j,
+			db = JSON.parse(Klarosk),
+			wordCount = [],
+			deckSize = 10,
+			bingoCardGenerated,
+			bingoCards = []
+		;
+
+		for (i = 0; i < db.length; i++)
+			wordCount[db[i].speech] = 0;
+
+		for (i = 0; i < deckSize; i++) {
+			bingoCardGenerated = new BingoCardCollection();
+			bingoCardGenerated.url = '../../../../../../../app/data/klarosk.json';
+			bingoCardGenerated.fetch({async: false});
+
+			bingoCards[i] = bingoCardGenerated.toJSON();
+		}
+
+		for (i = 0; i < deckSize; i++)
+			for (j = 0; j < bingoCards[i].length; j++)
+				wordCount[bingoCards[i][j].speech] += 1;
+
+		console.log(wordCount);
+	}
+
+	printDeck();
+
 	return describe('Play.', function() {
 		describe('Bingo Card Generator', function() {
-			// FIXME how many duplicateds? See requirements and implementation again!
-			it('Shouldn\'t have duplicate speechs (less than XXX% can be duplicated!)!', function() {
-				var i,
-					j,
-					db = JSON.parse(Klarosk),
-					wordCount = [],
-					deckSize = 10,
-					bingoCardGenerated,
-					bingoCards = []
-				;
-
-				for (i = 0; i < db.length; i++)
-					wordCount[db[i].speech] = 0;
-
-				for (i = 0; i < deckSize; i++) {
-					bingoCardGenerated = new BingoCardCollection();
-					bingoCardGenerated.url = '../../../../../../../app/data/klarosk.json';
-					bingoCardGenerated.fetch({async: false});
-
-					bingoCards[i] = bingoCardGenerated.toJSON();
-				}
-// console.log(bingoCards)
-
-				for (i = 0; i < deckSize; i++) {
-					for (j = 0; j < bingoCards[i].length; j++)
-						wordCount[bingoCards[i][j].speech] += 1;
-				}
-console.log(wordCount);
-
-				return true;
-			});
-
 			it('Shouldn\'t have duplicate speech in *SAME* bingo card.', function() {
 				return true;
 			});
