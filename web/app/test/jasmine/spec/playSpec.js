f2db99e8727482ad9d67ec0f50bdf8b02745c5ba
Jefferson Campos <foguinho.peruca@gmail.com>
2015-02-24 00:13:12 -0300

    Helper
    Update object definition

--- web/app/test/jasmine/spec/playSpec.js
+++ web/app/test/jasmine/spec/playSpec.js
@@ -1,16 +1,15 @@
 define([
 	'underscore'
-	, 'helper'
+	, '../../../test/jasmine/spec/specHelper'
 	, 'collections/bingo'
 	// , 'text!../../../../../../../test/data/timesheet/noDaysLater.json'
 	, 'text!../../../../../../../data/klarosk.json'
 ], function (_, Helper, BingoCardCollection, Klarosk) {
-
 	return describe('Play.', function() {
 		describe('Bingo Card Generator', function() {
 			it('Shouldn\'t have duplicate speech in *SAME* bingo card.', function() {
 				var helper = new Helper();
-				helper();
+				helper.printDeck();
 
 				return true;
 			});
--- web/app/test/jasmine/spec/specHelper.js
+++ web/app/test/jasmine/spec/specHelper.js
@@ -4,33 +4,34 @@ define([
 	// , 'text!../../../../../../../test/data/timesheet/noDaysLater.json'
 	, 'text!../../../../../../../data/klarosk.json'
 ], function (_, BingoCardCollection, Klarosk) {
+	var helper = function() {
+		this.printDeck = function() {
+			var i,
+				j,
+				db = JSON.parse(Klarosk),
+				wordCount = [],
+				deckSize = 10,
+				bingoCardGenerated,
+				bingoCards = []
+			;
 
-	var helper = function () {
-		var i,
-			j,
-			db = JSON.parse(Klarosk),
-			wordCount = [],
-			deckSize = 10,
-			bingoCardGenerated,
-			bingoCards = []
-		;
+			for (i = 0; i < db.length; i++)
+				wordCount[db[i].speech] = 0;
 
-		for (i = 0; i < db.length; i++)
-			wordCount[db[i].speech] = 0;
+			for (i = 0; i < deckSize; i++) {
+				bingoCardGenerated = new BingoCardCollection();
+				bingoCardGenerated.url = '../../../../../../../app/data/klarosk.json';
+				bingoCardGenerated.fetch({async: false});
 
-		for (i = 0; i < deckSize; i++) {
-			bingoCardGenerated = new BingoCardCollection();
-			bingoCardGenerated.url = '../../../../../../../app/data/klarosk.json';
-			bingoCardGenerated.fetch({async: false});
+				bingoCards[i] = bingoCardGenerated.toJSON();
+			}
 
-			bingoCards[i] = bingoCardGenerated.toJSON();
-		}
+			for (i = 0; i < deckSize; i++)
+				for (j = 0; j < bingoCards[i].length; j++)
+					wordCount[bingoCards[i][j].speech] += 1;
 
-		for (i = 0; i < deckSize; i++)
-			for (j = 0; j < bingoCards[i].length; j++)
-				wordCount[bingoCards[i][j].speech] += 1;
-
-		console.log(wordCount);
+			console.log(wordCount);
+		};
 	};
 
 	return helper;
