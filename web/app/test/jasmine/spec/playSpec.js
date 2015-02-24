b344c1cd798cf79ccd8e62f6d2d370b6135fd058
Jefferson Campos <foguinho.peruca@gmail.com>
2015-02-23 23:58:23 -0300

    Helper
    Moved to a util file

--- web/app/test/jasmine/spec/playSpec.js
+++ web/app/test/jasmine/spec/playSpec.js
@@ -1,43 +1,17 @@
 define([
 	'underscore'
-	, 'app'
+	, 'helper'
 	, 'collections/bingo'
 	// , 'text!../../../../../../../test/data/timesheet/noDaysLater.json'
 	, 'text!../../../../../../../data/klarosk.json'
-], function (_, App, BingoCardCollection, Klarosk) {
-	function printDeck() {
-		var i,
-			j,
-			db = JSON.parse(Klarosk),
-			wordCount = [],
-			deckSize = 10,
-			bingoCardGenerated,
-			bingoCards = []
-		;
-
-		for (i = 0; i < db.length; i++)
-			wordCount[db[i].speech] = 0;
-
-		for (i = 0; i < deckSize; i++) {
-			bingoCardGenerated = new BingoCardCollection();
-			bingoCardGenerated.url = '../../../../../../../app/data/klarosk.json';
-			bingoCardGenerated.fetch({async: false});
-
-			bingoCards[i] = bingoCardGenerated.toJSON();
-		}
-
-		for (i = 0; i < deckSize; i++)
-			for (j = 0; j < bingoCards[i].length; j++)
-				wordCount[bingoCards[i][j].speech] += 1;
-
-		console.log(wordCount);
-	}
-
-	printDeck();
+], function (_, Helper, BingoCardCollection, Klarosk) {
 
 	return describe('Play.', function() {
 		describe('Bingo Card Generator', function() {
 			it('Shouldn\'t have duplicate speech in *SAME* bingo card.', function() {
+				var helper = new Helper();
+				helper();
+
 				return true;
 			});
 		});
--- web/app/test/jasmine/spec/specHelper.js
+++ web/app/test/jasmine/spec/specHelper.js
@@ -1,12 +1,37 @@
-// beforeEach(function() {
-//   this.addMatchers({
-//     toBePlaying: function(expectedSong) {
-//       var player = this.actual;
-//       return player.currentlyPlayingSong === expectedSong && 
-//              player.isPlaying;
-//     }
-//   });
-// });
+define([
+	'underscore'
+	, 'collections/bingo'
+	// , 'text!../../../../../../../test/data/timesheet/noDaysLater.json'
+	, 'text!../../../../../../../data/klarosk.json'
+], function (_, BingoCardCollection, Klarosk) {
 
+	var helper = function () {
+		var i,
+			j,
+			db = JSON.parse(Klarosk),
+			wordCount = [],
+			deckSize = 10,
+			bingoCardGenerated,
+			bingoCards = []
+		;
 
-console.log('specHelper...');
+		for (i = 0; i < db.length; i++)
+			wordCount[db[i].speech] = 0;
+
+		for (i = 0; i < deckSize; i++) {
+			bingoCardGenerated = new BingoCardCollection();
+			bingoCardGenerated.url = '../../../../../../../app/data/klarosk.json';
+			bingoCardGenerated.fetch({async: false});
+
+			bingoCards[i] = bingoCardGenerated.toJSON();
+		}
+
+		for (i = 0; i < deckSize; i++)
+			for (j = 0; j < bingoCards[i].length; j++)
+				wordCount[bingoCards[i][j].speech] += 1;
+
+		console.log(wordCount);
+	};
+
+	return helper;
+});
